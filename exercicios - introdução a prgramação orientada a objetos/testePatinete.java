df2c2a69d41191ac2e0b21cbb62f2a1d2881e6da
Beatriz Paixão <biaperon2001@gmail.com>
2021-09-19 15:09:34 -0300

    Adicionando o restante dos exercicios

---
+++ exercicios - introdução a prgramação orientada a objetos/testePatinete.java
@@ -0,0 +1,17 @@
+package ExerciciosPOO;
+
+public class testePatinete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Patinete patinete1 = new Patinete("Atrio Fun","ES205","Preto/Vermelho",23,250);
+		patinete1.imprimir();
+		Patinete patinete2 = new Patinete("Foston","S08 Pro","Preto",25,250);
+		patinete2.imprimir();
+		Patinete patinete3 = new Patinete("Freego","ES06C","Cinza",24,150);
+		patinete3.imprimir();
+
+	}
+
+}
