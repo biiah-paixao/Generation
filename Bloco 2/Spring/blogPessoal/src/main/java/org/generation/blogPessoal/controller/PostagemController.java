3d5d430adf8e90c1e95eb0c5e500d5f0bbd22152
Beatriz Paixão <biaperon2001@gmail.com>
2021-09-27 12:25:35 -0300

    Atualizando dados

--- Bloco 2/Spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Bloco 2/Spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,9 +1,15 @@
 package org.generation.blogPessoal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +25,42 @@ public class PostagemController {
 	
 	//ingerar a classe de repository
 	@Autowired // responsabilidade de instanciação dessa interface para o spring - ingessão de dependencia
-	private PostagemRepository repository; // objeto da interface repository
+	private PostagemRepository postagemRepository; // objeto da interface repository
 	
 	//primeiro metodo - find all
 	@GetMapping
-	public ResponseEntity<List<Postagem>> GetAll() 
+	public ResponseEntity<List<Postagem>> getAll() 
 	{
-		return ResponseEntity.ok(repository.findAll());
+		return ResponseEntity.ok(postagemRepository.findAll());
 	} // findAll é igual a select * from tb_postagens
-
+	
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id) { 
+		return postagemRepository.findById(id)
+			.map(resposta -> ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}") // especifica é uma variavel do titulo {nome do titulo}
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping // é enviar dados e persistir (gravar) um dado no banco
+	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem) { //requestBody é para pegar o corpo todo da requisição
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping // ATUALIZAR um dado no banco
+	public ResponseEntity<Postagem> putPostagem(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable long id) { // void não tem retorno
+		postagemRepository.deleteById(id);
+	}
+	
 }
--- Bloco 2/Spring/blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ Bloco 2/Spring/blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
-	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo); //findAllBy - busca todos pelo      Titulo - nome do atributo da entidade      Containing  - like - trazer tudo que conter dentro dele     IgnoreCase - ignorar maiusculo e minusculo
+	public List <Postagem> findAllByTituloContainingIgnoreCase (String titulo); //findAllBy - busca todos pelo      Titulo - nome do atributo da entidade      Containing  - like - trazer tudo que conter dentro dele     IgnoreCase - ignorar maiusculo e minusculo
 
-}
+} //find = select/ all = * from / by = were 
+	// Containing = like "%titulo%"
+	//findAllByTituloContainingIgnoreCase (String titulo);
+	//traga tudo de que contem e ignore case (String titulo - tudo do titulo
+	
